a782c0c5525eb3b34735ea4065b4d48c01365370
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-05-26 09:56:29 -0600

    encapsulamiento

--- src/Persona.java
+++ src/Persona.java
@@ -1,5 +1,23 @@
 package src;
 
-public class Persona {
+public abstract class Persona {
+    private Integer id;
+    private String nombre;
+    private String apellidos;
+    private Integer edad;
+    
+    public Persona() {
+    }
+
+    public Persona(Integer id, String nombre, String apellidos, Integer edad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+    public abstract void concentrarse();
+
+    public abstract void viajar();
 
 }
