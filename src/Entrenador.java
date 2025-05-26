e0d711021da75bd86a9c9df42b7cf5da3c300beb
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-05-26 10:10:43 -0600

    se hizo en entrenador encapsulamiento y herencia. polimorfismo cambiando metodos de persona y la herencia que le dio persona

--- src/Entrenador.java
+++ src/Entrenador.java
@@ -1,5 +1,38 @@
 package src;
 
-public class Entrenador {
+public class Entrenador extends Persona {
+    private String idFederacion;
+
+    public Entrenador() {
+    }
+
+    public Entrenador(Integer id, String nombre, String apellidos, Integer edad, String idFederacion) {
+        super(id, nombre, apellidos, edad);
+        this.idFederacion = idFederacion;
+    }
+
+    public String getIdFederacion() {
+        return idFederacion;
+    }
+
+    public void setIdFederacion(String idFederacion) {
+        this.idFederacion = idFederacion;
+    }
+
+    @Override
+    public void concentrarse() {
+        System.out.println("El entrenador se concentra.");
+    }
+    @Override
+    public void viajar() {
+        System.out.println("El entrenador viaja con el equipo.");
+    }
+
+    public void dirigirPartido() {
+        System.out.println("El entrenador dirige un partido.");
+    }
+    public void dirigirEntrenamiento() {
+        System.out.println("El entrenador dirige un entrenamiento.");
+    }
 
 }
