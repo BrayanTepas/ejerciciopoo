4f245b5e0fd9f53c2c6cb60e6baafb7b52a2b3fe
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-05-26 10:21:05 -0600

    esta es la clase padre que hereda a las hijas futbolista etc y la abstraccion que modificaran las hijas para hacer polimorfismo

--- src/Persona.java
+++ src/Persona.java
@@ -1,5 +1,6 @@
 package src;
-
+//aqui se hizo la abstraccion de la clase Persona, que es la clase padre de Futbolista, Masajista y Entrenador
+//la herencia se da de la clase Persona a las clases Futbolista, Masajista y Entrenador
 public abstract class Persona {
     private Integer id;
     private String nombre;
@@ -13,6 +14,38 @@ public abstract class Persona {
         this.id = id;
         this.nombre = nombre;
         this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
         this.edad = edad;
     }
 
