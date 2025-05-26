1f153350bcb2f3989708eb52489977dfffc6d71e
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-05-26 10:18:32 -0600

    se hizo herencia y abstraccion en en masajista aparte se hizo encapsulamiento y polimorfismo cambiando los metodos de persona

--- src/Masajista.java
+++ src/Masajista.java
@@ -1,5 +1,52 @@
 package src;
 
-public class Masajista {
+public class Masajista extends Persona {
+    private String titulacion;
+    private Integer aniosExperiencia;
+
+    public Masajista() {
+    }
+
+    public Masajista(String titulacion, Integer aniosExperiencia) {
+        this.titulacion = titulacion;
+        this.aniosExperiencia = aniosExperiencia;
+    }
+
+    public Masajista(Integer id, String nombre, String apellidos, Integer edad, String titulacion,
+            Integer aniosExperiencia) {
+        super(id, nombre, apellidos, edad);
+        this.titulacion = titulacion;
+        this.aniosExperiencia = aniosExperiencia;
+    }
+
+    public String getTitulacion() {
+        return titulacion;
+    }
+
+    public void setTitulacion(String titulacion) {
+        this.titulacion = titulacion;
+    }
+
+    public Integer getAniosExperiencia() {
+        return aniosExperiencia;
+    }
+
+    public void setAniosExperiencia(Integer aniosExperiencia) {
+        this.aniosExperiencia = aniosExperiencia;
+    }
+
+    @Override
+    public void concentrarse() {
+        System.out.println("El masajista se concentra.");
+    }
+    
+    @Override
+    public void viajar() {
+        System.out.println("El masajista viaja con el equipo.");
+    }
+
+    public void darMasaje() {
+        System.out.println("El masajista da un masaje.");
+    }
 
 }
