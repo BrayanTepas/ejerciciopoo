2cac4e8e34a39e998488b01c33ee32f2f9562344
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-05-26 10:05:10 -0600

    futbolista herencia y encapsulamiento la abstraccion se hizo en persona y polimorfismo se sobreescribio los metodos de persona

--- src/Futbolista.java
+++ src/Futbolista.java
@@ -1,5 +1,53 @@
 package src;
 
-public class Futbolista {
+public class Futbolista extends Persona {
+    private Integer dorsal;
+    private String demarcacion;
 
+    public Futbolista() {
+    }
+
+    public Futbolista(Integer dorsal, String demarcacion) {
+        this.dorsal = dorsal;
+        this.demarcacion = demarcacion;
+    }
+
+    public Futbolista(Integer id, String nombre, String apellidos, Integer edad, Integer dorsal, String demarcacion) {
+        super(id, nombre, apellidos, edad);
+        this.dorsal = dorsal;
+        this.demarcacion = demarcacion;
+    }
+
+    // Getters and Setters
+    public Integer getDorsal() {
+        return dorsal;
+    }
+
+    public void setDorsal(Integer dorsal) {
+        this.dorsal = dorsal;
+    }
+
+    public String getDemarcacion() {
+        return demarcacion;
+    }
+
+    public void setDemarcacion(String demarcacion) {
+        this.demarcacion = demarcacion;
+    }
+
+    @Override
+    public void concentrarse() {
+        System.out.println("El futbolista se concentra.");
+    }
+    @Override
+    public void viajar() {
+        System.out.println("El futbolista viaja con el equipo.");
+    }
+    
+    public void jugarPartido() {
+        System.out.println("El futbolista juega un partido.");
+    }
+    public void entrenar() {
+        System.out.println("El futbolista entrena.");
+    }
 }
